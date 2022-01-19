06c0089ab20763ac6f4a48e58a779a2758681540
RafaelSabino <rafaelcruzsabiino@hotmail.com>
2022-01-19 08:17:07 -0300

    Criação Entities

---
+++ SeventhTest.API/SeventhTest.ENTITY/Servidor.cs
@@ -0,0 +1,15 @@
+﻿using SeventhTest.ENTITY.Base;
+
+namespace SeventhTest.ENTITY
+{
+    public class Servidor : BaseEntity
+    {
+        #region "Properties"
+
+        public string Nome     { get; set; }
+        public string Endereco { get; set; }
+        public int    Porta    { get; set; }
+
+        #endregion
+    }
+}
---
+++ SeventhTest.API/SeventhTest.ENTITY/Video.cs
@@ -0,0 +1,14 @@
+﻿using SeventhTest.ENTITY.Base;
+
+namespace SeventhTest.ENTITY
+{
+    public class Video : BaseEntity
+    {
+        #region "Properties"
+
+        public string IdServer  { get; set; }
+        public string Descricao { get; set; }
+
+        #endregion
+    }
+}
