29cf0ff2981a05f7e6400d7760fa3939ce116ac4
RafaelSabino <rafaelcruzsabiino@hotmail.com>
2022-01-24 20:07:56 -0300

    Final

--- SeventhdGuard.API/SeventhdGuard.BO/Interfaces/IReciclarBo.cs
+++ SeventhdGuard.API/SeventhdGuard.BO/Interfaces/IReciclarBo.cs
@@ -7,7 +7,7 @@ namespace SeventhdGuard.BO.Interfaces
 {
     public interface IReciclarBo
     {
-        Task RecyclerVideo(Reciclar entity, int days);
+        void RecyclerVideo(Reciclar entity, int days);
         bool Process(Reciclar entity, int days);
         ResultInfo BeginProcess(Reciclar entity);
         ResultInfo EndProcess(string id, DateTime dataFinished);
